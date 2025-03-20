8ae1494126d85a81731e8ced4320bbb782d10827
Marcos Gutiérrez <marcosgtzx7@hotmail.com>
2025-03-20 14:33:00 -0600

    Agregar archivos de proyecto.

---
+++ MVCG20250320S1.AppWebMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCG20250320S1.AppWebMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
