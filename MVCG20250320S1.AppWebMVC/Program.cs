30bf079a259c487814076c379e1156a851f0e2a0
Marcos Gutiérrez <marcosgtzx7@hotmail.com>
2025-03-24 14:47:20 -0600

    Se subvenb los ultimos cambios de git

--- MVCG20250320S1.AppWebMVC/Program.cs
+++ MVCG20250320S1.AppWebMVC/Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using MVCG20250320S1.AppWebMVC.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ClientesDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Conn"));
+});
 
 var app = builder.Build();
 
